b13f901d9680c532111cb9acb4e8ddc79575bd82
Pau Figueras <pau@pswsm.cat>
2022-02-09 20:34:07 +0100

    Ex5 finished

--- dictionaries/ex5.py
+++ dictionaries/ex5.py
@@ -2,4 +2,22 @@
 # @Date:   2022-02-09T19:50:38+01:00
 # @Email:  pau@pswsm.cat
 # @Last modified by:   pswsm
-# @Last modified time: 2022-02-09T19:50:38+01:00
+# @Last modified time: 2022-02-09T20:33:46+01:00
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", 'city', "salary"]
+
+
+def mkDictFromDict(ogDict, keysToExtract: list[str]) -> dict:
+    result = {}
+    for key in keysToExtract:
+        result[key] = ogDict[key]
+    return result
+
+
+print(mkDictFromDict(sample_dict, keys))
