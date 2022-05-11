8f2f3873477a0a2a9efd77e5cf1ee90bd1df930e
Pau Figueras <pau@pswsm.cat>
2022-05-11 20:39:27 +0200

    Refactor exercise

--- exceptions/e_b.py
+++ exceptions/e_b.py
@@ -1,8 +1,10 @@
 def check_seq(a) -> None:
     if not len(a) % 3 == 0:
-        print("Not triplets")
-
+        raise Exception
 
 if __name__ == "__main__":
     seq: str = input(f"Input a sequence:\n")
-    check_seq(seq)
+    try:
+        check_seq(seq)
+    except:
+        print(f"Not triplets")
