58af2822f92478f475387777b807acd394fed989
Pau Figueras <pau@pswsm.cat>
2022-05-19 17:31:40 +0200

    Added get parameters

--- web_dev/app/routes.py
+++ web_dev/app/routes.py
@@ -1,5 +1,5 @@
 """Routes for flask"""
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route("/")
@@ -7,6 +7,13 @@ from app import app
 def index():
     """Index handler"""
     return render_template('index.html')
+
+@app.route("/get")
+def get_params():
+    """Route that accepts parameters"""
+    par1 = request.args.get('p1')
+    par2 = request.args.get('p2')
+    return f"Param 1 = {par1}<br>Param 2 = {par2}"
 
 @app.errorhandler(404)
 def page_not_found(e):
